0613e9d73440d8a79625260edab39b59644f5056
viniciushenrique26 <viniciusslavov@gmail.com>
2024-01-09 12:33:48 -0300

    update challenges

---
+++ cpp/main.cpp
@@ -0,0 +1,23 @@
+
+// inclui biblioteca iostream
+#include <iostream> 
+
+
+// usar o namespace padrão
+using namespace std; //std = standard = padrão
+
+int main() { 
+
+
+    int anos; 
+
+    cout << "digite a sua idade: " << endl; 
+    cin >> anos; // indica que a idade é a entrada
+
+    cout << "Você nasceu em " << 2024 - anos << endl; 
+
+
+
+
+ return 0;
+}
